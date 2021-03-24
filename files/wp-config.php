f5d92988d0c92c2350989216d0f70c93ea4c33cb
sonaisrayel <israyelyan.sona@gmail.com>
2021-03-24 17:30:15 +0400

    add files

--- files/wp-config.php
+++ files/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress' );
+define( 'DB_USER', 'wpuser' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', 'wp-123456' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
